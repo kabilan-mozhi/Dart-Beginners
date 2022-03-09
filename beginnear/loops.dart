f87f9a2c283454e8aa861825ef0298a16dba017a
kabilan-mozhi <88093166+kabilan-mozhi@users.noreply.github.com>
2022-03-09 23:26:24 +0530

    try

--- beginnear/loops.dart
+++ beginnear/loops.dart
@@ -8,9 +8,33 @@ void main(List<String> args) {
     print(value);
     value++;
   } while (value < max);
-   //while (value <= max);
-   
-
+  //while (value <= max);
 
   print('Done do while loop');
+  value = init;
+  while (value <= max) {
+    print("the value in the while loop is ${value}");
+    value++;
+  }
+  print('Done with while loop');
+
+//infinity loop
+  /* value = init;
+  do {
+    print('value=${value}');
+    value++;
+  } while (true); */
+
+  value = init;
+  do {
+    print('value is${value}');
+    value++;
+    if (value == 3) {
+      print('value is 3');
+      continue;
+    }
+    if (value > 4) {
+      print('the =value ius greater than 4');
+    }
+  } while (true);
 }
